c250825a4eef82f61c2a2e1780573ce7c061b4ab
Krzych <krzych.pierzchala@gmail.com>
2017-10-25 07:27:36 +0200

    Set a basic version of project

---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+	var url = 'https://restcountries.eu/rest/v2/name/';
+	var $countryList = $('#countries');
+	var $btn = $('#search');
+	var $input = $('#country-name');
+
+	$btn.click(searchCountries);
+
+
+	function searchCountries() {
+		var $countryName = $input.val();
+		
+		if(!$countryName.length) $countryName = 'Poland';
+
+		$.ajax({
+			url: url + $countryName,
+			method: 'get',
+			success: showCountriesList
+		});
+	}
+
+	function showCountriesList(resp) {
+		$countryList.empty();
+		resp.forEach(function(item) {
+			$('<li>').text(item.name).appendTo($countryList);
+		});
+	}
+})
