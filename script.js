fc5e683a3220c7e00d9a06fbced93dc64846df19
Krzych <krzych.pierzchala@gmail.com>
2017-10-27 17:25:30 +0200

    Built a basic structure with working js for display spreed information about searched countries

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
 
 	$btn.click(searchCountries);
 
+	var countryPopulation = 0;
 
 	function searchCountries() {
 		var $countryName = $input.val();
@@ -18,13 +19,122 @@ $(document).ready(function () {
 			method: 'get',
 			success: showCountriesList
 		});
+
 	}
 
+	var list = [];
+/*
 	function showCountriesList(resp) {
 		$countryList.empty();
+		list = resp;
 		resp.forEach(function(item) {
 			var info = item.name + ', ' + item.capital;
+			/*var lang = item.languages.forEach(function(n) {
+				lang += n.name + ', '; 
+			})
 			$('<li>').text(info).appendTo($countryList);
+			console.log(item.languages[0].name);	 
 		});
 	}
+*/
+	function showCountriesList(resp) {
+		//$countryList.empty();
+		//console.log(resp);
+		clearContainer();
+		list = resp;
+		resp.forEach(function(item) {
+			//var name = item.name;
+			var boxInfo = new Info(item);
+			boxInfo.$element;
+			boxInfo.data;
+		});
+	}
+
+	function Info(input) {
+		var self = this;
+		this.object = input;
+		this.name = input.name;
+		this.flag = input.flag;
+		this.capital = input.capital;
+		this.area = input.area;
+		this.population = input.population;
+		this.lang = getLangName();
+		this.currency = getCurrencyName();
+		this.$element = createInfo();
+		this.data = fillData();
+
+		function createInfo() {
+	
+			$box = $('<div>').attr('class', 'box');
+			$flag = $('<img>').attr('class', 'flag');
+			$boxName = $('<h1>').attr('class', 'country-header').text(self.name);
+			$headerInner = $('<h2>').text('Background Information');
+			$inner = $('<div>');
+			$features = $('<ul>');
+
+			$flag.appendTo($box);
+			$boxName.appendTo($box);
+			$inner.append($headerInner);
+			$inner.append($features);
+			$box.append($inner);
+			$box.appendTo('.container');
+
+			createInformationList();
+		}
+
+		function createInformationList() {
+			//$headCountry = $('<li>').attr('class', 'headCountry').text('Head of State: ');
+			$capital = $('<li>').attr('class', 'capital');
+			$area = $('<li>').attr('class', 'area');
+			$population = $('<li>').attr('class', 'population'); 
+			$languages = $('<li>').attr('class', 'lang');
+			$currency = $('<li>').attr('class', 'currency').text('Currency: ');
+			
+
+			//$headCountry.appendTo($features);
+			$capital.appendTo($features);
+			$area.appendTo($features);
+			$population.appendTo($features);
+			$languages.appendTo($features);
+			$currency.appendTo($features);
+		}
+
+		function getLangName() {
+			var kurwa = "";	
+			input.languages.forEach(function(item) {
+				kurwa += item.name + ", ";
+			})
+			console.log(kurwa);
+			return kurwa;
+		}
+
+		function getCurrencyName() {
+			var kurwa = "";	
+			input.currencies.forEach(function(item) {
+				kurwa += item.name + ", ";
+			})
+			console.log(kurwa);
+			return kurwa;
+		}
+
+		function fillData() {
+			$flag.attr('src', self.flag);
+			console.log($flag);
+			$capital.text('Capital: ' + self.capital);
+			$area.text('Land area: ' + self.area);
+			$population.text('Population: ' + self.population);
+			$languages.text('Language(s): ' + self.lang);
+			$currency.text('Currency: ' + self.currency);
+		}
+	
+	}
+
+	function clearContainer () {
+		$('.container').empty();
+	}
+
+
+	//var test = new Info('Poland');
+	//console.log(test.name);
+	//console.log(test.lang);
 })
